38bbe7d4a754dd033b1e206e63c00a415cd1c7da
Mohammad Iqbal <iqbal240456@gmail.com>
2022-10-10 23:09:42 +0530

    Update Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import {Container , Card} from "react-bootstrap"
+import {Container , Card, Stack} from "react-bootstrap"
 
 export default function Feedback() {
   return (
     <>
      <Container  className='p-3' style={{backgroundColor:"#f8fafb" , height:"1000px" }}  fluid>
-        <Card>
-            <Card.Body style={{fontWeight:"bold"}}>Aromatic bar</Card.Body>
+        <Card style={{height:"40px" ,padding:"5px 0px 5px 15px"}}>
+            <Card.Title >Aromatic bar</Card.Title>
         </Card>
+        <Stack>
+
+        </Stack>
      </Container>
     </>
   )
