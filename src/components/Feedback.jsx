0f44f4ec690007e44a24577bd458fb309bdb3b96
Mohammad Iqbal <iqbal240456@gmail.com>
2022-10-11 12:11:32 +0530

    added check boxes

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Card, Stack, Form, Button } from "react-bootstrap";
-import {phoneData} from "../utils/phoneData"
+import { phoneData } from "../utils/phoneData";
 export default function Feedback() {
   return (
     <>
@@ -22,38 +22,98 @@ export default function Feedback() {
         >
           <Form>
             <Stack direction="horizontal" gap={5}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Customer Name<span style={{color:"red"}}>*</span></Form.Label>
-              <Form.Control type="text" placeholder="Eg. jhon doe" style={{width:"500px"}} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address<span style={{color:"red"}}>*</span></Form.Label>
-              <Form.Control type="email" placeholder="Enter email" style={{width:"500px"}} />
-            </Form.Group>
-
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label style={{ fontWeight: "bold" }}>
+                  Customer Name<span style={{ color: "red" }}>*</span>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Eg. jhon doe"
+                  style={{ width: "500px" }}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label style={{ fontWeight: "bold" }}>
+                  Email address<span style={{ color: "red" }}>*</span>
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  style={{ width: "500px" }}
+                />
+              </Form.Group>
             </Stack>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Phone<span style={{color:"red"}}>*</span></Form.Label>
-               <Stack direction="horizontal" gap={2}>
-               <Form.Select style={{width:"100px" ,fontSize:"16px"}}>
-                 {
-                 phoneData.map((opt)=>{
-                   return (
-                    <option  >
-                       {opt.name}
-                    </option>
-                   )
-                 })
-                 }
-              </Form.Select>
-              <Form.Control type="text" placeholder="Eg. 9999998988" style={{width:"390px"}} />
-
-               </Stack>
+              <Form.Label style={{ fontWeight: "bold" }}>
+                Phone<span style={{ color: "red" }}>*</span>
+              </Form.Label>
+              <Stack direction="horizontal" gap={2}>
+                <Form.Select style={{ width: "100px", fontSize: "16px" }}>
+                  {phoneData.map((opt) => {
+                    return <option>{opt.name}</option>;
+                  })}
+                </Form.Select>
+                <Form.Control
+                  type="text"
+                  placeholder="Eg. 9999998988"
+                  style={{ width: "390px" }}
+                />
+              </Stack>
+            </Form.Group>
+            <Stack direction="horizontal" gap={5}>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Label style={{ fontWeight: "bold" }}>
+                Please rate the quality of the service you recieved from the
+                host.<span style={{ color: "red" }}>*</span>
+              </Form.Label>
+              <Stack direction="horizontal" gap={5}>
+              <Form.Check type="checkbox" label="Excellent" />
+              <Form.Check type="checkbox" label="Good" />
+              <Form.Check type="checkbox" label="Fair" />
+              <Form.Check type="checkbox" label="Bad" />
+              </Stack>
+             
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+              <Form.Label style={{ fontWeight: "bold" }}>
+              Please rate the quality of your beverage.<span style={{ color: "red" }}>*</span>
+              </Form.Label>
+              <Stack direction="horizontal" gap={5}>
+              <Form.Check type="checkbox" label="Excellent" />
+              <Form.Check type="checkbox" label="Good" />
+              <Form.Check type="checkbox" label="Fair" />
+              <Form.Check type="checkbox" label="Bad" />
+              </Stack>
+             
             </Form.Group>
+            </Stack>
+            <Stack direction="horizontal" gap={5}>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Label style={{ fontWeight: "bold" }}>
+              Was our restaurant clean?<span style={{ color: "red" }}>*</span>
+              </Form.Label>
+              <Stack direction="horizontal" gap={5}>
+              <Form.Check type="checkbox" label="Excellent" />
+              <Form.Check type="checkbox" label="Good" />
+              <Form.Check type="checkbox" label="Fair" />
+              <Form.Check type="checkbox" label="Bad" />
+              </Stack>
+             
+            </Form.Group>
+            <Form.Group className="mb-3" style={{marginLeft:"100px"}}>
+              <Form.Label style={{ fontWeight: "bold" }}>
+              Please rate your overall dining experience.<span style={{ color: "red" }}>*</span>
+              </Form.Label>
+              <Stack direction="horizontal" gap={5}>
+              <Form.Check type="checkbox" label="Excellent" />
+              <Form.Check type="checkbox" label="Good" />
+              <Form.Check type="checkbox" label="Fair" />
+              <Form.Check type="checkbox" label="Bad" />
+              </Stack>
+             
+            </Form.Group>
+            </Stack>
             <Button variant="primary" type="submit">
               Submit
             </Button>
