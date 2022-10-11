2d9c6f6fcf3a47ab122b632675f7ff5731cf232d
Mohammad Iqbal <iqbal240456@gmail.com>
2022-10-11 14:32:49 +0530

    added table and titles

--- src/components/FeedbackDashboard.jsx
+++ src/components/FeedbackDashboard.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import {Container,Table} from "react-bootstrap"
+import {Container,Table ,Form} from "react-bootstrap"
 export default function FeedbackDashboard() {
   return (
     <>
-    <Container fluid  style={{ backgroundColor: "#f8fafb", height: "1000px" }}>
-          <h1>Dashboard</h1>
+    <Container fluid  style={{ backgroundColor: "#f8fafb", height: "1000px"  , padding:"40px"}}>
+   <h4 className='mb-3' style={{fontSize:"20px"}}>Aromatic bar</h4>
+   <p style={{fontSize:"15px" , fontFamily:"sans-serif"}}>112 Records found.</p>
+
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
     </Container>
     </>
   )
