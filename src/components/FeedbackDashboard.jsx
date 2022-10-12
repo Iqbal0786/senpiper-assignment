ba9b538446b89e4e20de3c270bf3c2cff9d1c4af
Mohammad Iqbal <iqbal240456@gmail.com>
2022-10-12 14:21:12 +0530

    Update FeedbackDashboard.jsx

--- src/components/FeedbackDashboard.jsx
+++ src/components/FeedbackDashboard.jsx
@@ -91,7 +91,7 @@ export default function FeedbackDashboard() {
               value={searchText}
               style={{ width: "200px" }}
               onChange={getSearchInput}
-              placeholder='search by name , email or phone'
+              placeholder='i.e name , email ,phone'
             />
             <i class="bi bi-search" onClick={searchHandler}></i>
             <Button
